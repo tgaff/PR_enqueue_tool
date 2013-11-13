e997fe6db3a81574715e00e3ac2eacafeed841c1
Travis Gaff <tgaff@verticalresponse.com>
2013-11-13 13:26:49 -0800

    Fix boolean tests and matching for minor issue in rails

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,6 +1,6 @@
 class Commit < ActiveRecord::Base
   belongs_to :pull_request
 
-  validates :test_pushed, presence: true
+  validates :test_pushed, inclusion: { in: [true, false] }
   validates :sha, length: { is: 40 }
 end
